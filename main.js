f7067261892303c945fcc67b0e7c67808bc9ed34
Moonlight404 <59587363+Moonlight404@users.noreply.github.com>
2020-07-06 17:00:19 -0300

    Initial

---
+++ main.js
@@ -0,0 +1,26 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        aprender: [
+            'A  linguagens dos cães.',
+            'Comportamento e humor do cão em cada situação.',
+            'Cuidados com seu cachorrinho.',
+            'Alimentos que podem estragar a saúde do seu cão.',
+            'Curiosidades sobre seu cão.',
+            'Como os cães aprendem.',
+            'Adestramento iniciante.',
+            'Adestramento avançado.',
+            'Cão velho aprende? Como adestrar?'
+        ],
+        menuOpened: true
+    },
+    methods: {
+        openMenu(){
+            if(this.menuOpened){
+                this.menuOpened = false
+            } else{
+                this.menuOpened = true
+            }
+        }
+    }
+})
